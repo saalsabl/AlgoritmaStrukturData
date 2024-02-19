e3c9d990d750a5d89471a51a822c7ed8da5f23ea
saalsabl <aisyahnzkk@gmail.com>
2024-02-19 10:50:15 +0700

    Perbaikan

--- Pertemuan 2/BukuMain01.java
+++ Pertemuan 2/BukuMain01.java
@@ -1,4 +1,3 @@
-
 public class BukuMain01 {
     public static void main(String[] args) {
         Buku01 bk1 = new Buku01();
@@ -12,7 +11,7 @@ public class BukuMain01 {
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
-
+        
         Buku01 bk2 = new Buku01("Self Reward", "Maheera Ayesha", 160, 29, 59000);
         bk2.terjual(11);
         bk2.tampilInformasi();
