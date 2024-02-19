6fbbe0555ee878140985328ad5cfeb095118f64a
saalsabl <aisyahnzkk@gmail.com>
2024-02-19 10:13:53 +0700

    Percobaan 1

---
+++ Pertemuan 2/Buku01.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Buku01{
+
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi(){
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman : " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
+    }
+
+    void terjual(int jml){
+        stok -= jml;
+    }
+
+    void restock(int jml){
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg){
+        harga = hrg;
+    }
+}
