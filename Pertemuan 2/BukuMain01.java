e64a0765e11236920044ce978d16e7b525784981
saalsabl <aisyahnzkk@gmail.com>
2024-02-19 10:23:51 +0700

    Percobaan 2

---
+++ Pertemuan 2/BukuMain01.java
@@ -0,0 +1,16 @@
+
+public class BukuMain01 {
+    public static void main(String[] args) {
+        Buku01 bk1 = new Buku01();
+        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1.pengarang = "Denada Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(60000);
+        bk1.tampilInformasi();
+    }
+}
