1cb71897a7cf6a1670109f22b1ac117c5698c6f2
saalsabl <aisyahnzkk@gmail.com>
2024-02-19 10:23:40 +0700

    merapikan

--- Pertemuan 2/Buku01.java
+++ Pertemuan 2/Buku01.java
@@ -5,11 +5,11 @@ public class Buku01{
     int halaman, stok, harga;
 
     void tampilInformasi(){
-        System.out.println("Judul: " + judul);
-        System.out.println("Pengarang: " + pengarang);
-        System.out.println("Jumlah halaman : " + halaman);
-        System.out.println("Sisa stok: " + stok);
-        System.out.println("Harga: Rp " + harga);
+        System.out.println("Judul           : " + judul);
+        System.out.println("Pengarang       : " + pengarang);
+        System.out.println("Jumlah halaman  : " + halaman);
+        System.out.println("Sisa stok       : " + stok);
+        System.out.println("Harga           : Rp " + harga);
     }
 
     void terjual(int jml){
