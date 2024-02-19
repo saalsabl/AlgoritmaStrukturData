93dfd659a0f6538aac9076d20294e927baeefef4
saalsabl <aisyahnzkk@gmail.com>
2024-02-19 14:24:36 +0700

    Tugas 1

--- Pertemuan 2/Buku01.java
+++ Pertemuan 2/Buku01.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 public class Buku01{
 
     String judul, pengarang;
-    int halaman, stok, harga;
+    int halaman, stok, harga, bayar, total, diskon;
 
     void tampilInformasi(){
         System.out.println("Judul           : " + judul);
         System.out.println("Pengarang       : " + pengarang);
         System.out.println("Jumlah halaman  : " + halaman);
         System.out.println("Sisa stok       : " + stok);
-        System.out.println("Harga           : Rp " + harga);
+        System.out.println("Harga 1 buku    : Rp " + harga);
+        System.out.println("Harga sebelum diskon : Rp " + total);
+        System.out.println("Harga setelah diskon : Rp " + bayar);
     }
 
     void terjual(int jml){
+        if (stok > 0){
         stok -= jml;
+        }
     }
 
     void restock(int jml){
@@ -24,6 +28,22 @@ public class Buku01{
         harga = hrg;
     }
 
+    void hitungHargaTotal(int jml){
+        total = harga * jml;
+    }
+
+    void hitungDiskon(){
+        if (total > 150000){
+            diskon += total * 0.12;
+        } else if (total > 75000 || total <= 150000){
+            diskon += total * 0.5;
+        }
+    }
+
+    void hitungHarga (){
+        bayar = total - diskon;
+       
+    }
     public Buku01() {
 
     }
--- Pertemuan 2/BukuMain01.java
+++ Pertemuan 2/BukuMain01.java
@@ -1,19 +1,26 @@
 public class BukuMain01 {
     public static void main(String[] args) {
-        Buku01 bk1 = new Buku01();
-        bk1.judul = "Today Ends Tomorrow Comes";
-        bk1.pengarang = "Denada Pratiwi";
-        bk1.halaman = 198;
-        bk1.stok = 13;
-        bk1.harga = 71000;
+        // Buku01 bk1 = new Buku01();
+        // bk1.judul = "Today Ends Tomorrow Comes";
+        // bk1.pengarang = "Denada Pratiwi";
+        // bk1.halaman = 198;
+        // bk1.stok = 13;
+        // bk1.harga = 71000;
+        // bk1.tampilInformasi();
 
-        bk1.tampilInformasi();
+        Buku01 bk1 = new Buku01("Today Ends Tomorrow Comes", "Denada Pratiwi", 198, 13, 71000);
         bk1.terjual(5);
         bk1.gantiHarga(60000);
+        bk1.hitungHargaTotal(5);
+        bk1.hitungDiskon();
+        bk1.hitungHarga();
         bk1.tampilInformasi();
-        
+
         Buku01 bk2 = new Buku01("Self Reward", "Maheera Ayesha", 160, 29, 59000);
         bk2.terjual(11);
+        bk2.hitungHargaTotal(11);
+        bk2.hitungDiskon();
+        bk2.hitungHarga();
         bk2.tampilInformasi();
     }
 }
