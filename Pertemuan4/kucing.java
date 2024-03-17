d9721d0fd2bc5847264bcbbe7693f02862772a91
saalsabl <aisyahnzkk@gmail.com>
2024-03-17 19:30:53 +0700

    kuis1

---
+++ Pertemuan4/kucing.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class kucing {
+    int pjgkuku;
+public static void main(String[] args) {
+    kucing[] kucingArray = new kucing[3];
+    for (int i = 0; i > 3; i++){
+        kucingArray[i] = new kucing();
+        System.out.println("Masukkan banyak kucing: ");
+        kucingArray[i] = sc.nextInt();
+        System.out.println("bany");
+    }
+
+}    
+}
+
