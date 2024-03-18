b26382faa942a5328164dd4ccd2c3e8b9a44e293
saalsabl <aisyahnzkk@gmail.com>
2024-03-18 10:16:00 +0700

    Percobaan 1

---
+++ Pertemuan5/Faktorial.java
@@ -0,0 +1,19 @@
+public class Faktorial {
+    public int nilai;
+    int FaktorialBF(int n){
+        int fakto = 1;
+        for(int i = 1; i <= n; i++){
+            fakto = fakto * i;
+        }
+        return fakto;
+    }
+    int FaktorialDC(int n){
+        if(n==1){
+            return 1;
+        }
+        else {
+            int fakto = n * FaktorialDC(n-1);
+            return fakto;
+        }
+    } 
+}
