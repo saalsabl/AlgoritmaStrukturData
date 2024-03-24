94f06f9e579ae3408014bd8f46c9a2a98e30330a
saalsabl <aisyahnzkk@gmail.com>
2024-03-24 22:12:53 +0700

    Modifikasi

--- Pertemuan5/MainPangkat.java
+++ Pertemuan5/MainPangkat.java
@@ -10,12 +10,36 @@ public class MainPangkat {
 
         Pangkat[]png = new Pangkat[elemen];
         for (int i = 0; i < elemen; i++){
-            png[i] = new Pangkat();
-            System.out.print("Masukkan nilai yang akan dipangkatkan ke-" + (i+1) + " : ");
-            png[i].nilai = sc.nextInt();
-            System.out.print("Masukkan nilai pemangkat ke-" + (i+1) + " : ");
-            png[i].pangkat = sc.nextInt();
+            int nilai = 0, pangkat = 0;
+            System.out.print("Masukkan nilai yang akan dipangkatkan : ");
+            nilai = sc.nextInt();
+            System.out.print("Masukkan nilai pangkat : ");
+            pangkat = sc.nextInt();
+            png[i] = new Pangkat(nilai, pangkat);
         }
+        System.out.println("PILIHAN MENU");
+        System.out.println("1. Brute Force");
+        System.out.println("2. Divide & Conquer");
+        System.out.println("Pilih menu 1/2 : ");
+        int menu = sc.nextInt();
+        switch (menu) {
+            case 1:
+                System.out.println("HASIL PANGKAT BRUTE FORCE");
+                for(int i = 0; i < elemen; i++){
+                    System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
+                }
+                break;
+            case 2:
+                System.out.println(" HASIL PANGKAT DIVIDE & CONQUER");
+                for (int i = 0; i < elemen; i++){
+                    System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+                }
+                break;
+
+            default:
+                System.out.println("Menu tidak dapat ditemukan.");
+        }
+
         System.out.println("=======================================");
         System.out.println("Hasil Pangkat dengan Brute Force ");
         for (int i = 0; i < elemen; i++){
--- Pertemuan5/Pangkat.java
+++ Pertemuan5/Pangkat.java
@@ -1,5 +1,10 @@
 public class Pangkat {
     public int nilai, pangkat;
+    Pangkat (int nilai, int pangkat){
+        this.nilai = nilai;
+        this.pangkat = pangkat;
+    }
+
     int pangkatBF (int a , int n){
         int hasil = 1;
         for (int i = 0; i < n; i++){
