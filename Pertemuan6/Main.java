087ac8f5a2956f04c66c990e14a29d4c8967f865
saalsabl <aisyahnzkk@gmail.com>
2024-03-25 10:09:18 +0700

    Perbaikan

--- Pertemuan6/Main.java
+++ Pertemuan6/Main.java
@@ -3,9 +3,9 @@ public class Main {
         DaftarMahasiswaBerprestasi list = new DaftarMahasiswaBerprestasi();
         Mahasiswa m1 = new Mahasiswa("Nusa", 2017, 25, 3);
         Mahasiswa m2 = new Mahasiswa("Rara", 2012, 19, 4);
-        Mahasiswa m3 = new Mahasiswa("Dompu", 2018, 19, 3.5);
+        Mahasiswa m3 = new Mahasiswa("Dompu", 2018, 19, 3);
         Mahasiswa m4 = new Mahasiswa("Abdul", 2017, 23, 2);
-        Mahasiswa m5 = new Mahasiswa("Ummi", 2019, 21, 3.75);
+        Mahasiswa m5 = new Mahasiswa("Ummi", 2019, 21, 3);
 
         list.tambah(m1);
         list.tambah(m2);
