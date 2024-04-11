b370ead6e87dc02592bc43f8dcf9d5c7e7d807f4
saalsabl <aisyahnzkk@gmail.com>
2024-04-11 21:06:31 +0700

    modifikasi dan latihan

--- Pertemuan7/BukuMain01.java
+++ Pertemuan7/BukuMain01.java
@@ -8,49 +8,50 @@ public class BukuMain01 {
         PencarianBuku01 data = new PencarianBuku01();
         int jumBuku = 5;
 
-        System.out.println("--------------------------------------------------------");
-        System.out.println("Masukkan data Buku secara Urut dari KodeBuku Terkecil : ");
+        System.out.println("----------------------------------------------");
+        System.out.println("Masukkan data buku secara Urut dari kode buku terkecil : ");
         for (int i = 0; i < jumBuku; i++) {
             System.out.println("-------------------");
             System.out.print("Kode Buku \t: ");
-            int kodeBuku = s.nextInt();
+            String kodeBuku = s1.nextLine();
             System.out.print("Judul buku \t: ");
             String judulBuku = s1.nextLine();
             System.out.print("Tahun Terbit \t: ");
             int tahunTerbit = s.nextInt();
             System.out.print("Pengarang \t: ");
             String pengarang = s1.nextLine();
-            System.out.print("Stock     \t: ");
+            System.out.print("Stock \t: ");
             int stock = s.nextInt();
 
             Buku01 m = new Buku01(kodeBuku, judulBuku, tahunTerbit, pengarang, stock);
             data.tambah(m);
         }
-
-        System.out.println("---------------------------------------------------------------");
-        System.out.println("Data keseluruhan Mahasiswa : ");
-
-        System.out.println(" --------------------------------------------------------------");
+        System.out.println("--------------------------------------------------");
         System.out.println("Data keseluruhan Buku : ");
         data.tampil();
-
-        System.out.println("------------------------------------------------------------------------- ");
-        System.out.println("-------------------------------------------------------------------------");
-        System.out.println("Pencarian Data: ");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Pencarian Data : ");
         System.out.println("Masukkan Kode Buku yang dicari: ");
         System.out.print("Kode Buku : ");
-        int cari = s.nextInt();
+        String cari = s1.nextLine();
         System.out.println("Menggunakan sequential Search");
         int posisi = data.FindSeqSearch(cari);
-        data.Tampilpoisisi(cari, posisi);
-
+        data.Tampilposisi(cari, posisi);
         data.TampilData(cari, posisi);
 
-        //Percobaan2
-        System.out.println("===============================");
+        Buku01 dataBuku = data.FindBuku(cari);
+        if (dataBuku != null){
+            dataBuku.tampilDataBuku();
+        } else {
+            System.out.println("Buku dengan kode " + cari + " tidak ditemukan.");
+        }
+
+        // Percobaan 2
+        System.out.println("======================");
         System.out.println("menggunakan binary search");
         posisi = data.FindBinarySearch(cari, 0, jumBuku - 1);
-        data.Tampilpoisisi(cari, posisi);
+        data.Tampilposisi(cari, posisi);
         data.TampilData(cari, posisi);
+        
     }
-}
+}    
---
+++ Pertemuan7/Laporan.md
@@ -0,0 +1,17 @@
+# Laporan Praktikum Pertemuan 7
+Nama          : Aaisyah Nursalsabiil
+
+NIM           : 2341720171
+
+Kelas / absen : 1H - TI / 01
+
+## 6.2. Searching / Pencarian Menggunakan Agoritma Sequential Search
+<img src="Percobaan1(1).png">
+<img src="Percobaan1(2).png">
+<img src="Percobaan1(3).png">
+
+### 6.2.3. Pertanyaan
+1. Jelaskan fungsi break yang ada pada method FindSeqSearch!
+2. Jika Data Kode Buku yang dimasukkan tidak terurut dari kecil ke besar. Apakah program masih dapat berjalan? Apakah hasil yang dikeluarkan benar? Tunjukkan hasil screenshoot untuk bukti dengan kode Buku yang acak. Jelaskan Mengapa hal tersebut bisa terjadi?
+3. Buat method baru dengan nama FindBuku menggunakan konsep sequential search dengan tipe method dari FindBuku adalah BukuNoAbsen. Sehingga Anda bisa memanggil method tersebut pada class BukuMain seperti gambar berikut :
+<img src="Pertanyaan1.3.png">
