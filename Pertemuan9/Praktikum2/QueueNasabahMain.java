d553fb5d491cd91ef12379b4eb92d83070809db8
saalsabl <aisyahnzkk@gmail.com>
2024-05-05 16:33:33 +0700

    Percobaan2

--- Pertemuan9/Praktikum2/QueueNasabahMain.java
+++ Pertemuan9/Praktikum2/QueueNasabahMain.java
@@ -8,7 +8,7 @@ public class QueueNasabahMain {
         System.out.println("2. Antrian keluar");
         System.out.println("3. Cek Antrian terdepan");
         System.out.println("4. Cek Semua Antrian");
-        System.out.println("5. Cek Antrian paling belakang");
+        System.out.println("5. Cek Antian paling belakang");
         System.out.println("--------------------");
     }
 
