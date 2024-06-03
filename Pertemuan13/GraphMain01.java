fd242f6d0bed2e2d85e1cb6633f477a17970330e
saalsabl <aisyahnzkk@gmail.com>
2024-06-03 13:34:15 +0700

    percobaan 2

--- Pertemuan13/GraphMain01.java
+++ Pertemuan13/GraphMain01.java
@@ -1,5 +1,6 @@
 public class GraphMain01 {
     public static void main(String[] args) throws Exception {
+        //Percobaan 1
         Graph01 gedung = new Graph01(6);
         gedung.addEdge(0, 1, 50);
         gedung.addEdge(0, 2, 100);
@@ -12,6 +13,19 @@ public class GraphMain01 {
 
         gedung.removeEdge(1, 3);
         gedung.printGraph();
+
+        //Percobaan 2
+        GraphMatriks01 gdg = new GraphMatriks01(4);
+        gdg.makeEdge(0, 1, 50);
+        gdg.makeEdge(1, 0, 60);
+        gdg.makeEdge(1, 2, 70);
+        gdg.makeEdge(2, 1, 80);
+        gdg.makeEdge(2, 3, 40);
+        gdg.makeEdge(3, 1, 90);
+        gdg.printGraph();
+        System.out.println("Hasil setelah penghapusan edge");
+        gdg.removeEdge(2, 1);
+        gdg.printGraph();
     }
 }
     
